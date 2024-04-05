ebf127057fafe96fd459b85bc5cd4ea28212740b
Waylon <waylon.woo36@gmail.com>
2024-04-04 19:25:48 -0700

    removing debug and outputs

---
+++ Examples/CAN_WRITE_EXAMPLE/CAN_WRITE_EXAMPLE/main.c
@@ -0,0 +1,34 @@
+/*
+ * CAN_WRITE_EXAMPLE.c
+ *
+ * Created: 4/4/2024 6:58:23 PM
+ * Author : Waylon
+ */ 
+
+#include "config.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include "can.h"
+#include "spi.h"
+
+int main(void)
+{
+	PRR &= ~(1<<PRSPI);
+	DDRC |= (1<<5);
+
+	MCP2515_init();
+	MCP2515_sendTestMessage();
+	
+	while(1)
+	{
+		MCP2515_sendTestMessage();
+		if(MCP2515_readReg(0x30) & (0x3<<5))
+		{
+			PORTC |= (1<<5);
+		}
+		_delay_ms(50);
+		PORTC &= ~(1<<5);
+		
+	}
+	return 0;
+}
